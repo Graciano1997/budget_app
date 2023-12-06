3d122a91719c424835021346c4fc23c8c9956370
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-12-06 22:24:29 +0000

    update operation_controller

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -10,9 +10,29 @@ class OperationsController < ApplicationController
     end
 
     def create
-
+       @operation=Operation.new(operation_params)
+       @operation.author=current_user
+       if @operation.save
+        @groups=Group.find(params[:operation][:category_ids])
+        @groups.each do |group_item|
+          @group_operation=GroupOperation.new
+          @group_operation.operation=@operation
+          @group_operation.group=group_item
+          @group_operation.save
+        end
+      redirect_to group_operations_path(group_id:params[:group_id])
+        # end
+      #  else
+       end
+      # puts "Aqui #{params[:operation][:category_ids]}"
     end
 
     def show
+    end
+
+    private 
+    
+    def operation_params
+      params.require(:operation).permit(:name,:amount,:category_ids)
     end
 end
